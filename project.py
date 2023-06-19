1a61261608f0d3710321f469635fb2324c8e619a
zubairrae <137037190+zubairrae@users.noreply.github.com>
2023-06-19 13:45:58 +0500

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import pyttsx3
 import base64
 
 # Initialize the text-to-speech engine
-engine = pyttsx3.init()
+engine = pyttsx3.init('espeak')
 
 # Get available voices
 voices = engine.getProperty('voices')
