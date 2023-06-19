045d9ad6e4e3e1e8ff5d32d33649a6312f138c6e
zubairrae <137037190+zubairrae@users.noreply.github.com>
2023-06-19 14:22:23 +0500

    Update project.py

--- project.py
+++ project.py
@@ -19,16 +19,18 @@ if uploaded_file is not None:
 
 # Convert text to speech
 if st.button("Convert to Speech"):
-    engine.save_to_file(text, "speech.mp3")
-    engine.runAndWait()
+    tts = gTTS(text)
+    fp = BytesIO()
+    tts.save(fp)
+    fp.seek(0)
+    audio_bytes = fp.read()
 
-    # Read the generated audio file
-    audio_file = open("speech.mp3", "rb")
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format="audio/mp3")
+    # Create an HTML audio player with autoplay enabled
+    b64 = base64.b64encode(audio_bytes).decode()
+    href = f'<audio controls autoplay><source src="data:audio/mp3;base64,{b64}" type="audio/mpeg"></audio>'
+    st.markdown(href, unsafe_allow_html=True)
 
     # Create a download link for the audio file
-    b64 = base64.b64encode(audio_bytes).decode()
     href = f'<a href="data:file/mp3;base64,{b64}" download="speech.mp3">Download Audio</a>'
     st.markdown(
         f'<div style="text-align:center">{href}</div>',
